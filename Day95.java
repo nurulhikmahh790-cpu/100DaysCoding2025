2e338e9b68d048c9b3d464a9a1913b8f42c2cc4c
nurulhikmahh790-cpu <nurulhikmahh790@gmail.com>
2025-12-12 20:48:16 +0800

    Create Day95.java

---
+++ Day95.java
@@ -0,0 +1,13 @@
+public class CetakDenganParameter {
+
+    // Method void dengan parameter
+    public static void cetakNama(String nama) {
+        System.out.println("Halo, " + nama + "!");
+    }
+
+    public static void main(String[] args) {
+
+        // Memanggil method dengan memberikan parameter
+        cetakNama("Nurul hikmah");
+    }
+}
